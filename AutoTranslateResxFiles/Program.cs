f58be719afac813fdbf5eaa1b183c609c5f1834f
Bruno Brito <bhdebrito@gmail.com>
2019-07-01 21:21:24 -0300

    a

---
+++ AutoTranslateResxFiles/Program.cs
@@ -0,0 +1,62 @@
+﻿using GoogleTranslateFreeApi;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoTranslateResxFiles
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cultureFrom = "en";
+            var folder =
+                new DirectoryInfo(@"D:\workspace\JPProject.IdentityServer4.AdminUI\src\Frontend\Jp.UI.SSO\Resources");
+            var culturesTo = "es;fr;ru;nl";
+
+            foreach (var cultureTo in culturesTo.Split(";"))
+            {
+                foreach (var resxFile in folder.GetFiles("*.resx", SearchOption.AllDirectories))
+                {
+                    if (resxFile.Name.Contains(cultureFrom))
+                        try
+                        {
+                            await Translate(resxFile, cultureFrom, cultureTo);
+
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                            throw;
+                        }
+                }
+            }
+        }
+
+        private static async Task Translate(FileInfo resxFile, string from, string to)
+        {
+            var content = root.Load(resxFile);
+            var destination = root.Load(resxFile);
+
+            var novoArquivo = Path.Combine(resxFile.Directory.FullName,
+                Path.GetFileNameWithoutExtension(resxFile.Name).Replace(from, to) + Path.GetExtension(resxFile.Name));
+
+            if (File.Exists(novoArquivo))
+                return;
+
+            var translator = new GoogleTranslator();
+
+            var languageFrom = GoogleTranslator.GetLanguageByISO(from.ToLower());
+            var languageTo = GoogleTranslator.GetLanguageByISO(to.ToLower());
+
+            foreach (var rootData in content.data)
+            {
+                var result = await translator.TranslateLiteAsync(rootData.value, languageFrom, languageTo);
+                destination.data.First(f => f.name == rootData.name).value = result.MergedTranslation;
+                Console.WriteLine($"{rootData.name} - {rootData.value} -> {result.MergedTranslation}");
+            }
+            destination.SerializeTo(novoArquivo);
+        }
+    }
+}
---
+++ AutoTranslateResxFiles/ResxModel.cs
@@ -0,0 +1,177 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace AutoTranslateResxFiles
+{
+
+
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class root
+    {
+        public root() { }
+        public static root Load(FileInfo stream)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(root));
+
+            return (root)serializer.Deserialize(stream.OpenRead());
+        }
+
+        public void SerializeTo(string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(root));
+            StreamWriter writer = new StreamWriter(path);
+            serializer.Serialize(writer.BaseStream, this);
+        }
+        private rootResheader[] resheaderField;
+
+        private rootData[] dataField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("resheader")]
+        public rootResheader[] resheader
+        {
+            get
+            {
+                return this.resheaderField;
+            }
+            set
+            {
+                this.resheaderField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("data")]
+        public rootData[] data
+        {
+            get
+            {
+                return this.dataField;
+            }
+            set
+            {
+                this.dataField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class rootResheader
+    {
+
+        private string valueField;
+
+        private string nameField;
+
+        /// <remarks/>
+        public string value
+        {
+            get
+            {
+                return this.valueField;
+            }
+            set
+            {
+                this.valueField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class rootData
+    {
+
+        private string valueField;
+
+        private string[] textField;
+
+        private string nameField;
+
+        private string spaceField;
+
+        /// <remarks/>
+        public string value
+        {
+            get
+            {
+                return this.valueField;
+            }
+            set
+            {
+                this.valueField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlTextAttribute()]
+        public string[] Text
+        {
+            get
+            {
+                return this.textField;
+            }
+            set
+            {
+                this.textField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute(Form = System.Xml.Schema.XmlSchemaForm.Qualified, Namespace = "http://www.w3.org/XML/1998/namespace")]
+        public string space
+        {
+            get
+            {
+                return this.spaceField;
+            }
+            set
+            {
+                this.spaceField = value;
+            }
+        }
+    }
+
+
+
+
+}
